d0cb4ac47e5a02a76c4aa9b620842bdb9bfcf79e
Kayden Cooper <kayden_cooper@yahoo.com>
2021-07-23 07:55:17 -0600

    couple minor updates

--- src/App.js
+++ src/App.js
@@ -42,8 +42,9 @@ class App extends React.Component {
                   <input type="number" name="donation" value={this.state.value} onChange={this.handleChange.bind(this)} className="form-control py-3 font-weight-bolder" placeholder="Donate Here" aria-label="$" aria-describedby="button-addon2" />
                 
                   <div className="input-group-append">
-                    <button disabled={false} className="btn btn-secondary py-3 hover" type="button" id="donation-button" onClick={()=> this.setState({progress: this.state.progress += this.state.value, donors: this.state.donors + 1, donations: this.state.donations -= this.state.value})}>Give Now</button>
-                  </div>
+                    <button disabled={false} className="btn btn-secondary py-3 hover" type="button" id="donation-button" onClick={()=> this.setState({progress: this.state.donations / 5000 * 100, donors: this.state.donors + 1, donations: this.state.donations - this.state.value})}>Give Now</button>
+                  
+                </div>
                 </div>
 
               </div>
@@ -52,8 +53,7 @@ class App extends React.Component {
         </div>
       </div> }
     else {
-      return (
-      <div className="App">
+      return <div className="App">
         <div className="row justify-content-center m-auto">
           <div className="col-12 col-md-5 bg-light p-4">
             <div className="card m-4 bg-lightblue">
@@ -62,17 +62,18 @@ class App extends React.Component {
             <div className="card m-4 p-3 ">
               <div className="card-body text-align-left">
                 <div className="progress progress-position progress-background">
-                  <div className="progress-bar" style={{ width: this.state.progress }} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="5000"></div>
+                  <div className="progress-bar" style={{ width: this.state.progress + '%'}} role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="5000"></div>
                 </div>
                 <h1 className="card-title h1-font">Only four days left to fund this project</h1>
                 <p className="font-size card-text">Join the <b>{this.state.donors }</b> other donors who have already supported this project.</p>
                 <div className="input-group mb-1">
                 
-                  <input type="number" name="donation" value={this.state.value} onChange={this.handleChange.bind(this)} className="form-control py-3 font-weight-bolder" placeholder="$" aria-label="$" aria-describedby="button-addon2" />
+                  <input type="number" name="donation" value={this.state.value} onChange={this.handleChange.bind(this)} className="form-control py-3 font-weight-bolder" placeholder="Donate Here" aria-label="$" aria-describedby="button-addon2" />
                 
                   <div className="input-group-append">
-                    <button disabled={true} className="btn btn-secondary py-3 hover" type="button" id="donation-button" onSubmit={this.submit.bind(this)}>Give Now</button>
-                  </div>
+                    <button disabled={true} className="btn btn-secondary py-3 hover" type="button" id="donation-button" onClick={()=> this.setState({progress: this.state.donations / 5000 * 100, donors: this.state.donors + 1, donations: this.state.donations - this.state.value})}>Give Now</button>
+                  
+                </div>
                 </div>
 
               </div>
@@ -80,7 +81,6 @@ class App extends React.Component {
           </div>
         </div>
       </div>
-    );
     }
 
     
